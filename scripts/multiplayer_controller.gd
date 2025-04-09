aad6232661c285b4b3b862a26633060d068a64cc
Xavier Dontigny <xavier.dontigny@umonreal.ca>
2025-04-09 17:02:32 -0400

    fixed camera

--- scripts/multiplayer_controller.gd
+++ scripts/multiplayer_controller.gd
@@ -9,6 +9,12 @@ var y_direction
 	set(id):
 		player_id = id
 		%InputSynchronizer.set_multiplayer_authority(player_id)
+		
+func _ready() -> void:
+	if multiplayer.get_unique_id() == player_id:
+		$Camera2D.make_current()
+	else:
+		$Camera2D.enabled = false
 
 func _apply_movement_from_input(delta):
 	x_direction = %InputSynchronizer.input_direction_x
